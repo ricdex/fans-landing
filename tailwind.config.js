b959ff847dfa72be2794342c692f4481c67ffb55
Ricardo Avila <ricdex@gmail.com>
2021-06-10 15:18:19 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+     purge: {
+      
+      content: ['./public/**/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'mapah': '#0BAAF7',
+        'mapahb': '#0C9CE2',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
